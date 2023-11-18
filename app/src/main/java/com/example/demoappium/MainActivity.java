00b4845289dd22aceaab566fa3ba4824cdc248b2
axing123 <chenxingxiong@chenxingxiongdeMacBook-Pro.local>
2023-11-18 21:30:46 +0800

    creat commit

---
+++ app/src/main/java/com/example/demoappium/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.demoappium;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+
+public class MainActivity extends AppCompatActivity {
+    /*@BindView(R.id.btn_longin)
+    ImageView btnLogin;
+*/
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+       // ButterKnife.bind(this);
+        findViewById(R.id.btn_longin).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,HomeActivity.class));
+            }
+        });
+
+    }
+
+    public void toHomeActivity() {
+
+    }
+
+
+}
